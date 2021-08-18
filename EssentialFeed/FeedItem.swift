f10f969395768e0129420141f6cc5eef8b31c5c7
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-08-18 13:40:44 +0700

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Bayu Kurniawan on 8/18/21.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
