cc294fbf9cf8ad57d69a63d38528c74b0692209e
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-08-23 11:36:50 +0700

    Simplify type declaration with a helper

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -38,7 +38,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   func test_load_deliversErrorOnClientError() {
     let (sut, client) = makeSUT()
     
-    expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.connectivity)) {
+    expect(sut, toCompleteWithResult: failure(.connectivity)) {
       let clientError = NSError(domain: "Test", code: 0)
       client.complete(with: clientError)
     }
@@ -50,7 +50,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     let samples = [199, 201, 300, 400, 500]
     
     samples.enumerated().forEach { index, code in
-      expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+      expect(sut, toCompleteWithResult: failure(.invalidData)) {
         let json = makeItemsJSON([])
         client.complete(withStatusCode: code, data: json, at: index)
       }
@@ -118,6 +118,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     trackForMemoryLeaks(sut, file: file, line: line)
     trackForMemoryLeaks(client, file: file, line: line)
     return (sut, client)
+  }
+  
+  private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+    return .failure(error)
   }
   
   private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
