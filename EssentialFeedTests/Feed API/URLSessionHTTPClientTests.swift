cd4e6a3802429ac2d10aff7bfad967d4e731ff12
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-08-23 13:44:52 +0700

    Creates `URLSessionDataTask` with URL on `URLSessionHTTPClient.get(from: URL)`

---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,45 @@
+//
+//  Created by Bayu on 8/23/21.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+  private let session: URLSession
+  
+  init(session: URLSession) {
+    self.session = session
+  }
+  
+  func get(from url: URL) {
+    session.dataTask(with: url) { _, _, _ in
+      
+    }
+  }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+  
+  func test_getFromURL_createsDataTaskWithURL() {
+    let url = URL(string: "https://any-url.com")!
+    let session = URLSessionSpy()
+    let sut = URLSessionHTTPClient(session: session)
+    
+    sut.get(from: url)
+    
+    XCTAssertEqual(session.receivedURLs, [url])
+  }
+  
+  // MARK: - Helpers
+  
+  private class URLSessionSpy: URLSession {
+    var receivedURLs = [URL]()
+
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+      receivedURLs.append(url)
+      return FakeURLSessionDataTask()
+    }
+  }
+  
+  private class FakeURLSessionDataTask: URLSessionDataTask {}
+}
