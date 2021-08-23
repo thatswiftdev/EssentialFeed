8a4b23f9d854d2d564fa344e2dfb85972c914fc8
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-08-23 16:54:03 +0700

    Guarantee correct URL and HTTP method on get request

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -23,6 +23,25 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
   
+  func test_getFromURL_performsGETRequestWithURL() {
+    URLProtocolStub.startInterceptingRequest()
+    
+    let client = URLSessionHTTPClient()
+    let url = URL(string: "https://any-url.com")!
+    let exp = expectation(description: "Wait for request")
+    
+    URLProtocolStub.observeRequests { request in
+      XCTAssertEqual(request.url, url)
+      XCTAssertEqual(request.httpMethod, "GET")
+      exp.fulfill()
+    }
+    
+    client.get(from: url) { _ in }
+    
+    wait(for: [exp], timeout: 1.0)
+    URLProtocolStub.stopInterceptingRequest()
+  }
+  
   func test_getFromURL_failsOnRequestError() {
    
     URLProtocolStub.startInterceptingRequest()
@@ -52,6 +71,7 @@ class URLSessionHTTPClientTests: XCTestCase {
   // MARK: - Helpers
   private class URLProtocolStub: URLProtocol {
     private static var stub: Stub?
+    private static var requestObserver: ((URLRequest) -> Void)?
     
     private struct Stub {
       let data: Data?
@@ -63,6 +83,10 @@ class URLSessionHTTPClientTests: XCTestCase {
       stub = Stub(data: data, response: response, error: error)
     }
     
+    static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+      requestObserver = observer
+    }
+    
     static func startInterceptingRequest() {
       URLProtocol.registerClass(URLProtocolStub.self)
     }
@@ -70,9 +94,11 @@ class URLSessionHTTPClientTests: XCTestCase {
     static func stopInterceptingRequest() {
       URLProtocol.unregisterClass(URLProtocolStub.self)
       stub = nil
+      requestObserver = nil
     }
     
     override class func canInit(with request: URLRequest) -> Bool {
+      requestObserver?(request)
       return true
     }
     
