05266652157e4b773f45e0de0b9e4f6549a80808
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-09-17 19:03:46 +0700

    Save command request cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -2,22 +2,54 @@
 // Copyright © 2021. All rights reserved.
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+  
+  private let store: FeedStore
+  
   init(store: FeedStore) {
-    
+    self.store = store
+  }
+  
+  func save(_ items: [FeedItem]) {
+    store.deleteCacheFeed()
   }
 }
 
 class FeedStore {
   var deleteCachedFeedCallCount = 0
+  
+  func deleteCacheFeed() {
+    deleteCachedFeedCallCount += 1
+  }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
+  
   func test_init_doesNotDeleteCacheUponCreation() {
     let store = FeedStore()
     _ = LocalFeedLoader(store: store)
     
     XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+  }
+  
+  func test_save_requestsCacheDeletion() {
+    let store = FeedStore()
+    let sut = LocalFeedLoader(store: store)
+    let items = [uniqueItem(), uniqueItem()]
+    
+    sut.save(items)
+    
+    XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+  }
+  
+  // MARK: -  Helpers
+  private func uniqueItem() -> FeedItem {
+    return FeedItem(id: UUID(), description: "Any", location: "Any", imageURL: anyURL())
+  }
+  
+  private func anyURL() -> URL {
+    return URL(string: "https://any-url.com")!
   }
 }
