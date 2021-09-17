a1745d59235bb17534e8202809d7ad09ccf161be
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-09-17 18:56:26 +0700

    Does not delete cache upon `LocalFeedLoader` creation

---
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -0,0 +1,23 @@
+// Created for EssentialFeed.
+// Copyright © 2021. All rights reserved.
+
+import XCTest
+
+class LocalFeedLoader {
+  init(store: FeedStore) {
+    
+  }
+}
+
+class FeedStore {
+  var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+  func test_init_doesNotDeleteCacheUponCreation() {
+    let store = FeedStore()
+    _ = LocalFeedLoader(store: store)
+    
+    XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+  }
+}
