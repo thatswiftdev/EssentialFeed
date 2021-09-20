d9db79bf0488c92f107ef2a5f05572fcd38f8082
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-09-20 16:58:18 +0700

    Extract cache insertion into a helper method to make logic inside closure callbacks easier to follow

--- EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -17,15 +17,19 @@ class LocalFeedLoader {
   func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
     store.deleteCacheFeed { [weak self] error in
       guard let self = self else { return }
-      
       if let cacheDeletionError = error {
         completion(cacheDeletionError)
       } else {
-        self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
-          guard self != nil else { return }
-          completion(error)
-        }
+        self.cache(items, with: completion)
       }
+    }
+  }
+  
+  private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+    store.insert(items, timestamp: self.currentDate()) { [weak self] error in
+      guard self != nil else { return }
+      
+      completion(error)
     }
   }
 }
