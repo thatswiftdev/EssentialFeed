65f4b0c0e9b25f4fd7eb982d3c4b6a88510520d5
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-09-24 14:34:59 +0700

    Extract `FeedStoreSpy` to a shared module scope

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -20,43 +20,4 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, store)
   }
-  
-  private class FeedStoreSpy: FeedStore {
-    
-    enum ReceivedMessage: Equatable {
-      case deleteCacheFeed
-      case insert([LocalFeedImage], Date)
-    }
-    
-    private var deletionCompletions = [DeletionCompletion]()
-    private var insertionCompletions = [InsertionCompletion]()
-    private(set) var receivedMessages = [ReceivedMessage]()
-    
-    func deleteCacheFeed(completion: @escaping DeletionCompletion) {
-      deletionCompletions.append(completion)
-      receivedMessages.append(.deleteCacheFeed)
-    }
-    
-    func completeDeletion(with error: Error, at index: Int = 0) {
-      deletionCompletions[index](error)
-    }
-    
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-      insertionCompletions.append(completion)
-      receivedMessages.append(.insert(feed, timestamp))
-    }
-    
-    func completeDeletionSuccessfully(at index: Int = 0) {
-      deletionCompletions[index](nil)
-    }
-    
-    func completeInsertion(with error: Error, at index: Int = 0) {
-      insertionCompletions[index](error)
-    }
-    
-    func completeInsertionSuccessfully(at index: Int = 0) {
-      insertionCompletions[index](nil)
-    }
-  }
-
 }
