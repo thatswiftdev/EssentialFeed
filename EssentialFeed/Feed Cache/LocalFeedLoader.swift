e1b281d0e728d6f9b146687d14828d34a90e99c8
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-09-28 16:41:01 +0700

    Make `LocalFeedLoader` conform to the `FeedLoader`

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -29,7 +29,7 @@ public final class LocalFeedLoader {
   }
 }
 
-extension LocalFeedLoader {
+extension LocalFeedLoader: FeedLoader {
   public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
     store.deleteCacheFeed { [weak self] error in
       guard let self = self else { return }
