b8b9650eb8681c8e7422d7444bb30e5a1717fa7d
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-10-05 14:10:07 +0700

    Extract duplicate delete test code into a reusable method

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -66,6 +66,11 @@ class CodableFeedStore {
   }
   
   func deleteCacheFeed(completion: @escaping FeedStore.DeletionCompletion) {
+    guard FileManager.default.fileExists(atPath: storeURL.path) else {
+      return completion(nil)
+    }
+    
+    try! FileManager.default.removeItem(at: storeURL)
     completion(nil)
   }
 }
@@ -161,14 +166,20 @@ class CodableFeedStoreTests: XCTestCase {
   
   func test_delete_hasNoSideEffectsOnEmptyCache() {
     let sut = makeSUT()
-    let exp = expectation(description: "Wait for cache deletion")
     
-    sut.deleteCacheFeed { deletionError in
-      XCTAssertNil(deletionError, "Expected empty cache deletion to succeed.")
-      exp.fulfill()
-    }
-    wait(for: [exp], timeout: 1.0)
+    let deletionError = deleteCache(from: sut)
     
+    XCTAssertNil(deletionError, "Expected empty cache deletion to succeed.")
+    expect(sut, toRetrieve: .empty)
+  }
+  
+  func test_delete_emptiesPreviouslyInsertedCache() {
+    let sut = makeSUT()
+    insert((uniqueImageFeed().local, Date()), to: sut)
+    
+    let deletionError = deleteCache(from: sut)
+    
+    XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed.")
     expect(sut, toRetrieve: .empty)
   }
   
@@ -177,6 +188,19 @@ class CodableFeedStoreTests: XCTestCase {
     let sut =  CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
     trackForMemoryLeaks(sut, file: file, line: line)
     return sut
+  }
+  
+  private func deleteCache(from sut: CodableFeedStore) -> Error? {
+    let exp = expectation(description: "Wait for cache deletion.")
+    var deletionError: Error?
+    
+    sut.deleteCacheFeed { receivedDeletionError in
+      deletionError = receivedDeletionError
+      exp.fulfill()
+    }
+    wait(for: [exp], timeout: 1.0)
+    
+    return deletionError
   }
   
   @discardableResult
