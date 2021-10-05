712ec0dbbffc47edbf33c57856c8c11b4171103e
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-10-05 11:31:30 +0700

    Insert delivers error on insertion error (invalid store URL)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -54,11 +54,15 @@ class CodableFeedStore {
   }
   
   func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-    let encoder = JSONEncoder()
-    let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-    let encoded = try! encoder.encode(cache)
-    try! encoded.write(to: storeURL)
-    completion(nil)
+    do {
+      let encoder = JSONEncoder()
+      let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+      let encoded = try encoder.encode(cache)
+      try encoded.write(to: storeURL)
+      completion(nil)
+    } catch {
+      completion(error)
+    }
   }
 }
 
@@ -140,6 +144,17 @@ class CodableFeedStoreTests: XCTestCase {
     expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
   }
   
+  func test_insert_deliversErrorOnInsertionError() {
+    let invalidStoreURL = URL(string: "invalid://store-url")!
+    let sut = makeSUT(storeURL: invalidStoreURL)
+    let feed = uniqueImageFeed().local
+    let timestamp = Date()
+    
+    let insertionError = insert((feed, timestamp), to: sut)
+    
+    XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error.")
+  }
+  
   // MARK: -  Helpers
   private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
     let sut =  CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
@@ -153,7 +168,6 @@ class CodableFeedStoreTests: XCTestCase {
     
     var capturedError: Error?
     sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
-      XCTAssertNil(insertionError, "Expected feed to be inserted successfully.")
       capturedError = insertionError
       exp.fulfill()
     }
