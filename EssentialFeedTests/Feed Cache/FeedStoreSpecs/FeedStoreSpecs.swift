617709e71e33ade2155eb161654e9f52be6ea858
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-10-07 17:07:23 +0700

    Update `FeedStoreSpecs` (assertion separation from expectation)

--- EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -9,11 +9,15 @@ protocol FeedStoreSpecs {
   func test_retrieve_deliversFoundValuesOnNonEmptyCache()
   func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
 
+  func test_insert_deliversNoErrorOnEmptyCache()
+  func test_insert_deliversNoErrorOnNonEmptyCache()
   func test_insert_overridesPreviouslyInsertedCacheValues()
 
+  func test_delete_deliversNoErrorOnEmptyCache()
   func test_delete_hasNoSideEffectsOnEmptyCache()
+  func test_delete_deliversNoErrorOnNonEmptyCache()
   func test_delete_emptiesPreviouslyInsertedCache()
-  
+
   func test_storeSideEffects_runSerially()
 }
 
